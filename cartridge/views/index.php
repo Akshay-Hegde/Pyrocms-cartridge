cffd7e2e7c3ad0900171c5510b75a063ebbff4d9
lanuser <lanuser@lanuser.ru>
2014-03-18 17:44:29 +0400

    jquery fix

--- cartridge/views/index.php
+++ cartridge/views/index.php
@@ -1,3 +1,15 @@
+<script>
+	$(document).ready(function(){
+		var cont = <?= $count_active?>;
+		if (cont == 5) {
+			$('#cid').prop('disabled', true);
+			$('#count').prop('disabled', true);
+			$('#comment').prop('disabled', true);
+			$('#btnSave').prop('disabled', true);
+			alert('Вы заказали максимальное количесво картриджей, перейдите в раздел "Ваши заявки"');
+		}
+	});
+</script>
 <h2 id="page_title" class="page-title"><?= lang('frontend:cartridge:index:title');?></h2>
 <div>
 	<?php if(validation_errors()):?>
@@ -10,18 +22,18 @@
 	<p>
 		<label for="cartridge_id"><?= lang('frontend:cartridge:index:cart_need');?>:</label><br/>
 		<? $data = array('' => lang('frontend:buttons:dropdown')); foreach ($cartridges as $row) { $data[$row->id] = $row->name; } ?>
-		<?= form_dropdown('cartridge_id', $data, ''); ?>
+		<?= form_dropdown('cartridge_id', $data, '', 'id="cid"'); ?>
 	</p>
 
 	<p>
-				<label for="count_request"><?=lang('frontend:cartridge:index:cart_count');?> (<?=lang('frontend:cartridge:index:can_to_order');?> <?= $count_active; ?>)</label><br/>
-			<? $data = array('' => lang('frontend:buttons:dropdown')); for ($i = 1; $i <= $count_active; $i++) { $data[$i] = $i; } ?>
-			<?= form_dropdown('count_request', $data, ''); ?>
+		<label for="count_request"><?=lang('frontend:cartridge:index:cart_count');?> (<?=lang('frontend:cartridge:index:can_to_order');?> <?= $count_active; ?>)</label><br/>
+		<? $data = array('' => lang('frontend:buttons:dropdown')); for ($i = 1; $i <= $count_active; $i++) { $data[$i] = $i; } ?>
+		<?= form_dropdown('count_request', $data, '', 'id="count"'); ?>
 	</p>
 	<p>
-				<label for="comment"><?= lang('frontend:cartridge:index:comment');?></label>
-				<?php echo form_textarea(array('name' => 'comment', 'cols' => 20, 'rows' => 5)); ?>
+		<label for="comment"><?= lang('frontend:cartridge:index:comment');?></label>
+		<?php echo form_textarea(array('name' => 'comment', 'cols' => 20, 'rows' => 5, "id" => "comment")); ?>
 	</p>
-        <?= form_submit('', lang('frontend:buttons:save')); ?>
+        <?= form_submit('', lang('frontend:buttons:save'), 'id="btnSave"'); ?>
         <?= form_close(); ?>
 </div>
